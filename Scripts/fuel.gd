eed38d4fa377fb9356f34eb64971e4ea1cef99f2
iuK-BCanedo <brunocanedocarvalho@gmail.com>
2024-10-08 15:33:20 -0300

    commit inicial

---
+++ Scripts/fuel.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+const FALL_SPEED = 100.0
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	print("+7 fuel")
+	queue_free()
+
+func _physics_process(delta: float) -> void:
+	position.y += FALL_SPEED * delta
+	
+	if position.y > 720:
+		queue_free()
